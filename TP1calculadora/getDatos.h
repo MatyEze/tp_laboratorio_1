b554fc72d6eb558836a38fa673f0c847aafe2216
MatyEze <matiasezequielsellanes@hotmail.com>
2020-04-09 22:32:48 -0300

    primer commit

---
+++ TP1calculadora/getDatos.h
@@ -0,0 +1,30 @@
+
+
+/** \brief = getFloat(char[]); toma un flotante
+ *
+ * \param char[] = mensaje de pedido
+ * \return float
+ *
+ */
+float getFloat(char[]);
+
+/** \brief getFloatMin(char[], float, char[]); toma un flotante y lo valida con un minimo
+ *
+ * \param char[] = mensaje de pedido
+ * \param float = el minimo para validar
+ * \param char[] = mensaje de error
+ * \return float
+ *
+ */
+float getFloatMin(char[], float, char[]);
+
+/** \brief getFloatMax(char[], float, char[]); toma un flotante y lo valida con un maximo
+ *
+ * \param char[] = mensaje de pedido
+ * \param float = el maximo para validar
+ * \param char[] = mensaje de error
+ * \return float
+ *
+ */
+float getFloatMax(char[], float, char[]);
+float getFloatMinMax(char[], float, float, char[]);
