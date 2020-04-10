aca68d666d9487d378b6848b72167e40501ba2ac
MatyEze <matiasezequielsellanes@hotmail.com>
2020-04-10 00:41:38 -0300

    Agrego menu y toma de datos

--- TP1calculadora/main.c
+++ TP1calculadora/main.c
@@ -4,9 +4,41 @@
 
 int main()
 {
-    float num;
-    num = getFloat("Ingresa numero: ");
+ int opcion;
+ float numeroA;
+ float numeroB;
 
-    printf("%f",num);
+    do
+    {
+        printf("\n1. Cambiar numero A (%f)", numeroA);
+        printf("\n2. Cambiar numero B (%f)", numeroB);
+        printf("\n3. Calcular operaciones.");
+        printf("\n4. Mostrar resultados.");
+        printf("\n5. Salir.\n");
+        printf("\nIngrese una opcion: ");
+        scanf("%d", &opcion);
 
+        switch(opcion)
+        {
+            case 1:
+                numeroA=getFloat("\nIngrese el numero A: ");
+            break;
+            case 2:
+                numeroB=getFloat("\nIngrese el numero B: ");
+            break;
+            case 3:
+
+            break;
+            case 4:
+
+            break;
+        }
+     //system("pause");
+     system("cls");
+
+    }while(opcion!=5);
+
+
+
+    return 0;
 }
